ed84a12467bddadd12253e6ad6f7b1f546618b88
e7rick <161330845+e7rick@users.noreply.github.com>
2024-05-28 22:23:47 -0300

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -5,167 +5,248 @@
 #include "keyboard.h"
 #include "screen.h"
 
-#define WIDTH 20
-#define HEIGHT 20
-#define SCORE_FILE "scores.txt"
+#define largura 60
+#define altura 30
+#define arquivoScore "score.txt"
+#define velocidadeInicial 200000 
+#define velocidadePosmaca 250000 
 
-typedef struct Point {
-    int x, y;
-} Point;
+#define corverde "\x1b[32m"
+#define corvermelha "\x1b[31m"
+#define resetarRestante "\x1b[0m"
 
-typedef struct SnakeSegment {
-    Point position;
-    struct SnakeSegment *next;
-} SnakeSegment;
+typedef struct Ponto {
+ int x, y;
+} Ponto;
+
+typedef struct CaudaCobra {
+ Ponto posicao;
+ struct CaudaCobra *ptr;
+} CaudaCobra;
 
 typedef struct {
-    SnakeSegment *head;
-    SnakeSegment *tail;
-    int length;
-    Point direction;
-} Snake;
+ CaudaCobra *cabeca;
+ CaudaCobra *cauda;
+ int comprimento;
+ Ponto direcao;
+} Cobra;
 
 typedef struct {
-    Point position;
-} Food;
+ Ponto posicao;
+} Comida;
 
-void initGame(Snake *snake, Food *food) {
-    SnakeSegment *headSegment = (SnakeSegment *)malloc(sizeof(SnakeSegment));
-    headSegment->position.x = WIDTH / 2;
-    headSegment->position.y = HEIGHT / 2;
-    headSegment->next = NULL;
 
-    snake->head = headSegment;
-    snake->tail = headSegment;
-    snake->length = 1;
-    snake->direction.x = 0;
-    snake->direction.y = 0;
+void salvarPontuacao(int pontuacao);
+void carregarPontuacoes();
+void alocarMemoriaCobra(Cobra *cobra);
 
-    food->position.x = rand() % WIDTH;
-    food->position.y = rand() % HEIGHT;
+void start(Cobra *cobra, Comida *comida, int *velocidade) {
+CaudaCobra *cabecaCauda =(CaudaCobra*)malloc(sizeof(CaudaCobra));
+    
+ cabecaCauda -> posicao.x = largura / 2;
+ cabecaCauda -> posicao.y = altura / 2;
+ cabecaCauda-> ptr = NULL;
+
+ cobra -> cabeca = cabecaCauda;
+ cobra -> cauda = cabecaCauda;
+ cobra-> comprimento = 1;
+ cobra-> direcao.x = 1; 
+ cobra -> direcao.y = 0;
+
+ comida-> posicao.x = rand() % largura;
+ comida-> posicao.y = rand() % altura;
+
+ *velocidade = velocidadeInicial;
 }
 
-void drawGame(Snake *snake, Food *food) {
+void borda(Cobra *cobra, Comida *comida) {
     screenClear();
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            int isSnake = 0;
-            SnakeSegment *current = snake->head;
-            while (current != NULL) {
-                if (current->position.x == j && current->position.y == i) {
-                    screenGotoxy(j + SCRSTARTX, i + SCRSTARTY);
-                    printf("#");
-                    isSnake = 1;
-                    break;
-                }
-                current = current->next;
-            }
-            if (!isSnake) {
-                if (food->position.x == j && food->position.y == i) {
-                    screenGotoxy(j + SCRSTARTX, i + SCRSTARTY);
-                    printf("O");
-                }
-            }
-        }
+
+ printf(" ");
+ for (int i = 0; i < largura; i++) {
+   printf("#");
+ }
+   printf("\n");
+
+for (int i = 0; i < altura; i++) {
+        printf("#");
+    
+ for (int j = 0; j < largura; j++) {
+  int posicapCobra = 0;
+  CaudaCobra *atual = cobra -> cabeca;
+     
+while (atual != NULL) {
+    
+ if (atual ->posicao.x == j && atual -> posicao.y == i) {
+  printf(corverde "x" resetarRestante);
+     
+    posicapCobra = 1;
+    break;
+ }
+atual = atual -> ptr;
+}
+if (!posicapCobra) {
+    if (comida -> posicao.x == j && comida -> posicao.y == i) {
+    printf(corvermelha "O" resetarRestante);
+     } else {
+     printf(" ");
+     }
     }
+   }
+    printf("#\n");
+}
+
+
+    printf(" ");
+    for (int i = 0; i < largura; i++) {
+    printf("#");
+    }
+    printf("\n");
+}
+
+void atualizarCobra(Cobra *cobra) {
+CaudaCobra * novaCabeca = (CaudaCobra *) malloc (sizeof(CaudaCobra));
+    novaCabeca-> posicao.x = cobra -> cabeca -> posicao.x + cobra->direcao.x;
+    novaCabeca->posicao.y = cobra -> cabeca-> posicao.y + cobra-> direcao.y;
+    novaCabeca-> ptr = cobra->cabeca;
+    cobra -> cabeca = novaCabeca;
+
+if (novaCabeca ->  posicao.x < 0 || novaCabeca -> posicao.x >= largura || novaCabeca-> posicao.y < 0 || novaCabeca->  posicao.y >= altura) {
+printf("Game over ! pontuação: %d\n", cobra->comprimento - 1);
+ salvarPontuacao(cobra-> comprimento - 1); 
+  printf("\nPontuações anteriores:\n");
+ carregarPontuacoes(); 
+ alocarMemoriaCobra(cobra); 
+ exit(0); 
+    }
+}
+
+void paraFruta(Cobra *cobra, Comida *comida, int *velocidade) {
+if (cobra -> cabeca->posicao.x == comida->posicao.x && cobra ->cabeca-> posicao.y == comida ->posicao.y) {
+ cobra -> comprimento++;
+ comida -> posicao.x = rand() % largura;
+ comida -> posicao.y = rand() % altura;
+  if (*velocidade > velocidadePosmaca) {
+    *velocidade = *velocidade - velocidadePosmaca;
+  } else {
+  *velocidade = velocidadePosmaca;
+}
+} else {
+   CaudaCobra *corpo = cobra->cabeca;
+    
+    while (corpo -> ptr ->ptr != NULL) {
+    corpo = corpo-> ptr;
+}
+        
+    free(corpo -> ptr);
+    corpo-> ptr= NULL;
+    cobra ->cauda = corpo;
+    }
+}
+
+void salvarPontuacao(int pontuacao) {
+ FILE *arquivo = fopen(arquivoScore, "a");
+ 
+  if (arquivo == NULL) {
+  fprintf(stderr, "erro ao abrir o arquivo: %s\n", arquivoScore);
+  return;
+}
+  fprintf(arquivo, "%d\n", pontuacao);
+  fclose(arquivo);
+}
+
+void carregarPontuacoes() {
+ FILE *arquivo = fopen(arquivoScore, "r");
+ if (arquivo == NULL) {
+  fprintf(stderr, "erro ao abrir o arquivo: %s\n", arquivoScore);
+  return;
+}
+  int pontuacao;
+  while (fscanf(arquivo, "%d", &pontuacao) != EOF) {
+   printf("%d ", pontuacao);
+}
+    fclose(arquivo);
+}
+
+void tela() {
+    screenClear();
     screenUpdate();
 }
 
-void updateSnake(Snake *snake) {
-    SnakeSegment *newHead = (SnakeSegment *)malloc(sizeof(SnakeSegment));
-    newHead->position.x = snake->head->position.x + snake->direction.x;
-    newHead->position.y = snake->head->position.y + snake->direction.y;
-    newHead->next = snake->head;
-    snake->head = newHead;
-
-    if (newHead->position.x >= WIDTH) newHead->position.x = 0;
-    if (newHead->position.x < 0) newHead->position.x = WIDTH - 1;
-    if (newHead->position.y >= HEIGHT) newHead->position.y = 0;
-    if (newHead->position.y < 0) newHead->position.y = HEIGHT - 1;
-}
-
-void checkFood(Snake *snake, Food *food) {
-    if (snake->head->position.x == food->position.x && snake->head->position.y == food->position.y) {
-        snake->length++;
-        food->position.x = rand() % WIDTH;
-        food->position.y = rand() % HEIGHT;
-    } else {
-        SnakeSegment *current = snake->head;
-        while (current->next->next != NULL) {
-            current = current->next;
-        }
-        free(current->next);
-        current->next = NULL;
-        snake->tail = current;
-    }
-}
-
-void saveScore(int score) {
-    FILE *file = fopen(SCORE_FILE, "a");
-    if (file == NULL) {
-        perror("Error opening file");
-        return;
-    }
-    fprintf(file, "%d\n", score);
-    fclose(file);
-}
-
-void screenInitGame() {
+void menuPrincipal() {
     screenClear();
-    screenBoxEnable();
-    for (int i = SCRSTARTY; i <= SCRENDY; i++) {
-        screenGotoxy(SCRSTARTX, i);
-        printf("%c", BOX_VLINE);
-        screenGotoxy(SCRENDX, i);
-        printf("%c", BOX_VLINE);
+    printf("Bem vindo ao jogo da cobrinha !\n\n");
+    printf("pressione qualquer botao para começar :)\n");
+}
+
+void alocarMemoriaCobra(Cobra *cobra) {
+    CaudaCobra *corpo = cobra ->cabeca;
+    while (corpo  != NULL) {
+        CaudaCobra *ptr = corpo -> ptr;
+        free(corpo ); 
+        corpo =ptr;
     }
-    for (int j = SCRSTARTX; j <= SCRENDX; j++) {
-        screenGotoxy(j, SCRSTARTY);
-        printf("%c", BOX_HLINE);
-        screenGotoxy(j, SCRENDY);
-        printf("%c", BOX_HLINE);
-    }
-    screenGotoxy(SCRSTARTX, SCRSTARTY);
-    printf("%c", BOX_UPLEFT);
-    screenGotoxy(SCRENDX, SCRSTARTY);
-    printf("%c", BOX_UPRIGHT);
-    screenGotoxy(SCRSTARTX, SCRENDY);
-    printf("%c", BOX_DWNLEFT);
-    screenGotoxy(SCRENDX, SCRENDY);
-    printf("%c", BOX_DWNRIGHT);
-    screenBoxDisable();
-    screenUpdate();
+    cobra -> cabeca = NULL;
+    cobra -> cauda = NULL;
 }
 
 int main() {
-    Snake snake;
-    Food food;
-    char command;
-    int score = 0;
+    Cobra cobra;
+    Comida comida;
+    char teclapre;
+    int pontuacao = 0;
+    int velocidade;
 
     srand(time(NULL));
-    initGame(&snake, &food);
     keyboardInit();
-    screenInitGame();
+    tela();
 
-    while (1) {
-        if (keyhit()) {
-            command = readch();
-            switch (command) {
-                case 'w': snake.direction.x = 0; snake.direction.y = -1; break;
-                case 's': snake.direction.x = 0; snake.direction.y = 1; break;
-                case 'a': snake.direction.x = -1; snake.direction.y = 0; break;
-                case 'd': snake.direction.x = 1; snake.direction.y = 0; break;
-                case 'q': saveScore(score); return 0;
-            }
+while (1) {
+ menuPrincipal();
+ getchar(); 
+ start(&cobra, &comida, &velocidade);
+
+ while (1) {
+  if (keyhit()) {
+    teclapre = readch();
+    switch (teclapre) {
+    case 'w':
+    if (cobra.direcao.y == 0) {
+     cobra.direcao.x = 0;
+      cobra.direcao.y = -1;
+ }
+  break;
+     case 's':
+      if (cobra.direcao.y == 0) {
+      cobra.direcao.x = 0;
+      cobra.direcao.y = 1;
+}
+  break;
+     case 'a':
+      if (cobra.direcao.x == 0) {
+      cobra.direcao.x = -1;
+      cobra.direcao.y = 0;
+}
+ break;
+     case 'd':
+      if (cobra.direcao.x == 0) {
+      cobra.direcao.x = 1;
+     cobra.direcao.y = 0;
+}
+ break;
+     case 'q':
+     salvarPontuacao(pontuacao);
+     alocarMemoriaCobra(&cobra);
+    return 0;
+ }
+}
+     atualizarCobra(&cobra);
+     paraFruta(&cobra, &comida, &velocidade);
+     borda(&cobra, &comida);
+     usleep(velocidade); 
+     pontuacao = cobra.comprimento - 1;
         }
-        updateSnake(&snake);
-        checkFood(&snake, &food);
-        drawGame(&snake, &food);
-        usleep(100000);  // 100 ms delay
-        score = snake.length - 1;
-    }
+}
 
     return 0;
 }
-
